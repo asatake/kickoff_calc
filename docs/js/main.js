e5adbafeb65289d857c380d4148ba5e642e8718b
asatake <asataken@gmail.com>
2017-10-06 13:55:32 +0900

    点数計算機能搭載

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,7 +4,16 @@
     const STORE_NAME = "points";
     const team = 10;
     const first_point = 10;
-    var db;
+
+    // ページロード時に残ってしまっているデータベースを削除
+    var req = indexedDB.deleteDatabase(DB_NAME);
+    req.onsuccess = function(evt) {
+        // console.log(req.result);
+        console.log('init success.');
+    };
+    req.onerror = function(evt) {
+        console.log('init failure');
+    };
 
     var request = indexedDB.open(DB_NAME, 1);
     request.onerror = function(evt) {
@@ -21,8 +30,9 @@
         var store = evt.currentTarget.result.createObjectStore(
             STORE_NAME, { keyPath: "id" });
         store.createIndex('id', 'id', { unique: true });
+        // 初期値入力
         var first = [];
-        for (var i = 1; i < team; i++) {
+        for (var i = 0; i < team; i++) {
             first.push({id: i, point: first_point});
         }
         for (var x in first) {
@@ -73,20 +83,80 @@ function getBlob(key, store, success_callback) {
     };
 }
 
-openDb();
+// DBがopen済みでないならopen
+if(typeof indexedDB.instance !== 'undefined') {
+    openDb();
+}
 
-// 計算の主処理
-function calc(){
-    var points = [];
+// 確認ダイアログ
+function calcConfirm() {
+    if (window.confirm('OK?')) {
+        main();
+    }
+    else {
+        console.log('calc canceled.');
+    }
+}
+
+function main() {
+    var pointArray;
     var table = getObjectStore(STORE_NAME, "readwrite");
-    table.openCursor().onsuccess = function(evt) {
-        var cursor = evt.target.result;
-        if (cursor) {
-            console.log("id: " + cursor.key + ", point: " + cursor.value.point);
-            cursor.continue();
-        }
-        else {
-            console.log('no more entries');
+    var req = table.getAll();
+    req.onerror = function(evt) {
+        console.log('get failed');
+    };
+    req.onsuccess = function(evt) {
+        var result = calc(evt.target.result);
+        for (var x in result) {
+            var r = table.put(result[x]);
+            r.onsuccess = function() {
+                console.log('put success');
+            };
+            r.onerror = function() {
+                console.log('put failed');
+            };
         }
     };
+    return pointArray;
+}
+
+// 計算の主処理
+function calc(pointArray){
+    var ods = [
+        {one: 2, two: 4, three: 6, four: 8},
+        {one: 2, two: 4, three: 6, four: 8},
+        {one: 2, two: 4, three: 6, four: 8},
+        {one: 2, two: 4, three: 6, four: 8},
+    ];
+    var result = [];
+    var battle = document.getElementById('battle'); // n回戦
+    var f = document.getElementById('first'); // n番が1位だった
+    for(var i in pointArray) {
+        var point = pointArray[i].point;
+        var sel = document.getElementById('sel' + i); // n番目を選んでいる
+        var latch = document.getElementById('latch' + i); // 掛け金
+        point -= latch.value;
+        if (sel.selectedIndex === f.selectedIndex) {
+            switch (Number(sel.selectedIndex)) {
+            case 0:
+                point += latch.value * ods[battle.selectedIndex].one;
+                break;
+            case 1:
+                point += latch.value * ods[battle.selectedIndex].two;
+                break;
+            case 2:
+                point += latch.value * ods[battle.selectedIndex].three;
+                break;
+            case 3:
+                point += latch.value * ods[battle.selectedIndex].four;
+                break;
+            default:
+                point += 0;
+            }
+        }
+        var tpoint = document.getElementById('tpoint' + i);
+        tpoint.innerHTML = point;
+        result.push({'id': Number(i), point: Number(point)});
+    }
+    return result;
 }
